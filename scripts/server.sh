4ffd37ea3327f5c05e720a275d2d2e586d53b16e
Mark.Wang <kugouming@users.noreply.github.com>
2024-08-28 20:49:23 +0800

    Update server.sh

--- scripts/server.sh
+++ scripts/server.sh
@@ -12,6 +12,26 @@ while true; do
     fi
 done
 
-echo -e "\n\n当前目录为：\n\t$(pwd)\n\nWebServer: http://$(hostname -i | awk '{print $NF}'):${port}\n\n"
 
-python -m SimpleHTTPServer $port > /dev/null 2>&1 &
+PYTHON_BIN=python
+version=$(python -V 2>&1 | grep ^Python)
+if [[ $? -ne 0 ]]; then
+	version=$(python3 -V 2>&1 | grep ^Python)
+	if [[ $? -eq 0 ]]; then
+		PYTHON_BIN=python3
+	fi
+fi
+
+if [[ ${#}version -eq 0 ]]; then
+	echo "No available Python environment!"
+	exit 1
+fi
+
+ver=$(echo "${version}" | tr . ' ' | awk '{print $2}')
+if [[ $ver -eq 2 ]]; then
+	${PYTHON_BIN} -m SimpleHTTPServer $port > /dev/null 2>&1 &
+else
+	${PYTHON_BIN} -m http.server $port > /dev/null 2>&1 &
+fi
+
+echo -e "\n\n当前目录为：\n\t$(pwd)\n\nWebServer: http://$(hostname -i | awk '{print $NF}'):${port}\n\n"
